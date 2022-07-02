b52ea773251ee445a18fb8110a3ba91f1aa05ac7
Prime Inspire <60377115+Prime-Inspire@users.noreply.github.com>
2022-07-02 13:00:50 +0530

    Add files via upload

---
+++ enable-stuff.js
@@ -0,0 +1,6 @@
+// Toolip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+// Popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
